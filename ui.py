30ae7d66cac12ee04cbc8654721c63f5ad352330
ChXWu <cxwu5130@gmail.com>
2018-07-26 01:05:03 -0400

    update the UI

--- ui.py
+++ ui.py
@@ -36,37 +36,40 @@ class FinalProject(App):
         # build buttons with random color from the dictionary
         for i in range(nrows):
             for j in range(ncols):
-                btn_matrix[i][j] = Button(text = '({},{})'.format(i,j))
+                btn_matrix[i][j] = Button()#text = '({},{})'.format(i,j))
                 btn_matrix[i][j].background_normal = ''
-                self.intialize_color(btn_matrix, color_dict, i,j)
+                self.intialize_btn_color(btn_matrix, color_dict, i,j)
                 #btn_matrix[i][j].background_color = color_dict[r(0,len(color_dict)-1)]
                 display_layout.add_widget(btn_matrix[i][j])
         return btn_matrix
 
-    def intialize_color(self,btn_matrix, color_dict, i,j):
+    def intialize_btn_color(self,btn_matrix, color_dict, i,j):
         used_bc = []
         if i < 2:
             pass
         elif btn_matrix[i-1][j].background_color == btn_matrix[i-2][j].background_color:
-            print (btn_matrix[i-1][j].background_color)
+            #print (btn_matrix[i-1][j].background_color)
             used_bc.append(btn_matrix[i-1][j].background_color)
         if j < 2:
             pass
         elif btn_matrix[i][j-1].background_color == btn_matrix[i][j-2].background_color:
-            print (btn_matrix[i][j-1].background_color)
+            #print (btn_matrix[i][j-1].background_color)
             used_bc.append(btn_matrix[i][j-1].background_color)
         for bc in used_bc:
-            color_dict.remove(bc)
+            try:
+                color_dict.remove(bc)
+            except:
+                pass
         btn_matrix[i][j].background_color = color_dict[r(0,len(color_dict)-1)]
         for bc in used_bc:
             color_dict.append(bc)
 
     def build(self):
-        title = Label(text = 'Eliminates the Jewels',size_hint_y = .05)
+        title = Label(text = 'Eliminates the Jewels',size_hint_y = .1,font_size='25sp')
 
         # layout for the result section
-        result_layout = BoxLayout(orientation = 'horizontal',size_hint_y = .1)
-        score = Label(text = 'Score: 0')
+        result_layout = BoxLayout(orientation = 'horizontal',size_hint_y = .05)
+        score = Label(text = 'Score : 0')
         time_passed = Label(text = 'Time Passed: 0')
         time_remaining = Label(text = 'Time Remaining: 100')
         result_layout.add_widget(score)
@@ -79,6 +82,7 @@ class FinalProject(App):
         ncols = 10
         display_layout = GridLayout(padding = 10, spacing = 10, cols = ncols, rows = nrows)
 
+        #initialize tht board
         self.intialize_board(display_layout, nrows, ncols)
 
 
